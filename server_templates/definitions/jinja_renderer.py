62f9d3ca30c8125737d282748ad8f9d4dfce2cf6
Curt Cox <curtcox@gmail.com>
2025-09-21 21:39:23 -0500

    Add jinja template example.

---
+++ server_templates/definitions/jinja_renderer.py
@@ -0,0 +1,18 @@
+# ruff: noqa: F821, F706
+# This template executes inside the Viewer runtime where `request` and `context` are provided.
+from jinja2 import Environment, FunctionLoader
+import urllib.request, json
+
+def load_from_url(name):
+    with urllib.request.urlopen(name) as r:
+        return r.read().decode("utf-8")
+
+env = Environment(loader=FunctionLoader(load_from_url))
+template = request.get('form_data').get('template')
+tpl = env.get_template(template)
+
+values = request.get('form_data').get('values')
+with urllib.request.urlopen(values) as r:
+    data = json.loads(r.read().decode("utf-8"))
+
+return {'output': tpl.render(data)}
